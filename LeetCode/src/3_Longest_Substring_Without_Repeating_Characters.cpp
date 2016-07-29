f1fa3db4153229de57b3c2809c389f24a4e7a4fb
crawler99 <taojiang99@gmail.com>
2016-07-29 15:46:04 +0800

    small change

---
+++ LeetCode/src/3_Longest_Substring_Without_Repeating_Characters.cpp
@@ -0,0 +1,53 @@
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <vector>
+
+class Solution_1
+{
+    public:
+        int lengthOfLongestSubstring(std::string s)
+        {
+            if (s.empty()) return 0;
+
+            std::vector<int> pMap(256, -1);
+
+            int idx1 = 0;
+            int idx2 = 0;
+            int max = 1;
+            pMap[s[idx1]] = 0;
+
+            while (true)
+            {
+                while ((++idx2 < s.size()) && (-1 == pMap[s[idx2]]))
+                {
+                    pMap[s[idx2]] = idx2;
+                }
+                max = std::max(max, idx2 - idx1);
+                if (idx2 < s.size())
+                {
+                    do
+                    {
+                        pMap[s[idx1]] = -1;
+                    }
+                    while (++idx1 <= pMap[s[idx2]]);
+                    pMap[s[idx2]] = idx2;
+                }
+                else
+                {
+                    return max;
+                }
+            }
+        }
+};
+
+int main()
+{
+    std::string testStr("abcabcbb");
+
+    Solution_1 slu1;
+    printf("Result: %d\n", slu1.lengthOfLongestSubstring(testStr));
+
+    return 0;
+}
+
