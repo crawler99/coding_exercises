2153547ab1b468189fa19aac8c6dc3b86ca2c3f3
crawler99 <taojiang99@gmail.com>
2017-08-18 14:43:42 +0800

    adding a script to do cmake

---
+++ LeetCode/cmake_debug.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+BUILD_DIR="build"
+mkdir -p ${BUILD_DIR}
+
+cd ${BUILD_DIR}
+rm -fr debug
+mkdir debug
+cd debug
+
+cmake                                  \
+  -DCMAKE_BUILD_TYPE=Debug             \
+  -DGTEST_ROOT=${RS_BUILDKIT}/gtest    \
+  -DGMOCK_ROOT=${RS_BUILDKIT}/gmock    \
+  -DGCC_ROOT=${RS_BUILDKIT}/gcc        \
+  ../..
