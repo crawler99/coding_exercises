7dbcb25f7cc8eb769560326349d6f2588742c834
crawler99 <taojiang99@gmail.com>
2017-08-22 13:05:12 +0800

    small change

--- LeetCode/4_Median_of_Two_Sorted_Arrays/test/testAll.cxx
+++ LeetCode/4_Median_of_Two_Sorted_Arrays/test/testAll.cxx
@@ -10,38 +10,24 @@ TEST(Correctness, SimpleInput)
     EXPECT_EQ(2.5, slu1.findMedianSortedArrays({1,2}, {3,4}));
     EXPECT_EQ(2.5, slu2.findMedianSortedArrays({1,2}, {3,4}));
 
-    std::vector<int> nums3 {1, 3, 10, 15};
-    std::vector<int> nums4 {2, 12};
     EXPECT_EQ(6.5, slu1.findMedianSortedArrays({1,3,10,15}, {2,12}));
     EXPECT_EQ(6.5, slu2.findMedianSortedArrays({1,3,10,15}, {2,12}));
 
-    std::vector<int> nums5 {};
-    std::vector<int> nums6 {1};
     EXPECT_EQ(1, slu1.findMedianSortedArrays({}, {1}));
     EXPECT_EQ(1, slu2.findMedianSortedArrays({}, {1}));
 
-    std::vector<int> nums7 {1, 2};
-    std::vector<int> nums8 {3, 5, 10, 20, 40};
     EXPECT_EQ(5, slu1.findMedianSortedArrays({1,2}, {3,5,10,20,40}));
     EXPECT_EQ(5, slu2.findMedianSortedArrays({1,2}, {3,5,10,20,40}));
 
-    std::vector<int> nums9 {1};
-    std::vector<int> nums10 {1};
     EXPECT_EQ(1, slu1.findMedianSortedArrays({1}, {1}));
     EXPECT_EQ(1, slu2.findMedianSortedArrays({1}, {1}));
 
-    std::vector<int> nums11 {1, 2};
-    std::vector<int> nums12 {1, 2, 3};
     EXPECT_EQ(2, slu1.findMedianSortedArrays({1,2}, {1,2,3}));
     EXPECT_EQ(2, slu2.findMedianSortedArrays({1,2}, {1,2,3}));
 
-    std::vector<int> nums13 {100001};
-    std::vector<int> nums14 {100000};
     EXPECT_EQ(100000.5, slu1.findMedianSortedArrays({100001}, {100000}));
     EXPECT_EQ(100000.5, slu2.findMedianSortedArrays({100001}, {100000}));
 
-    std::vector<int> nums15 {1};
-    std::vector<int> nums16 {2, 3, 4};
     EXPECT_EQ(2.5, slu1.findMedianSortedArrays({1}, {2,3,4}));
     EXPECT_EQ(2.5, slu2.findMedianSortedArrays({1}, {2,3,4}));
 }
