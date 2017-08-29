a67873fe8f5fc62c35e93b425b4d478230a9cad5
crawler99 <taojiang99@gmail.com>
2017-08-29 16:05:05 +0800

    small change

--- LeetCode/7_Reverse_Integer/test/testAll.cxx
+++ LeetCode/7_Reverse_Integer/test/testAll.cxx
@@ -4,17 +4,25 @@
 
 TEST(Correctness, SimpleInput)
 {
-    Solution s;
+    Solution1 s1;
+    Solution2 s2;
 
-    EXPECT_EQ(321, s.reverse(123));
-    EXPECT_EQ(-321, s.reverse(-123));
+    EXPECT_EQ(321, s1.reverse(123));
+    EXPECT_EQ(321, s2.reverse(123));
+    EXPECT_EQ(-321, s1.reverse(-123));
+    EXPECT_EQ(-321, s2.reverse(-123));
 
-    EXPECT_EQ(301, s.reverse(103));
-    EXPECT_EQ(-301, s.reverse(-103));
+    EXPECT_EQ(301, s1.reverse(103));
+    EXPECT_EQ(301, s2.reverse(103));
+    EXPECT_EQ(-301, s1.reverse(-103));
+    EXPECT_EQ(-301, s2.reverse(-103));
 
-    EXPECT_EQ(3, s.reverse(300));
-    EXPECT_EQ(-3, s.reverse(-300));
+    EXPECT_EQ(3, s1.reverse(300));
+    EXPECT_EQ(3, s2.reverse(300));
+    EXPECT_EQ(-3, s1.reverse(-300));
+    EXPECT_EQ(-3, s2.reverse(-300));
 
-    EXPECT_EQ(0, s.reverse(2147483647));
+    EXPECT_EQ(0, s1.reverse(2147483647));
+    EXPECT_EQ(0, s2.reverse(2147483647));
 }
 
