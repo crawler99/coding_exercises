185fc89bcec790a0c3fe0b78c88d88c26bb93f67
crawler99 <taojiang99@gmail.com>
2017-09-01 09:53:54 +0800

    small change

--- LeetCode/6_ZigZag_Conversion/impl.hpp
+++ LeetCode/6_ZigZag_Conversion/impl.hpp
@@ -9,6 +9,34 @@ class Solution
 public:
     std::string convert(std::string s, int numRows) 
     {
-        return s;
+	std::string rst;
+
+        int divider = numRows > 1 ? (2 * (numRows - 1)) : 1;
+        int numVerticalCols = ceil(double(s.size()) / divider);
+
+	auto appendChar = [&s,&rst](int idx)
+        {
+            if ((idx >=0) && (idx < s.size()))
+            {
+                rst.push_back(s[idx]);
+            }
+        };
+
+        for (int i = 0; i < numRows; ++i)
+        {
+            for (int j = 0; j <= numVerticalCols; ++j)
+            {
+                int idx1 = j * divider;
+                int idx0 = idx1 - i;
+                int idx2 = idx1 + i;
+                if ((i > 0) && (i < numRows - 1))
+                {
+                    appendChar(idx0);
+                }
+                appendChar(idx2);
+            }
+        }
+
+        return rst;
     }
 };
--- LeetCode/6_ZigZag_Conversion/test/testAll.cxx
+++ LeetCode/6_ZigZag_Conversion/test/testAll.cxx
@@ -8,5 +8,10 @@ TEST(Correctness, SimpleInput)
 
     EXPECT_EQ("PAHNAPLSIIGYIR", s.convert("PAYPALISHIRING", 3));
     EXPECT_EQ("PINALSIGYAHRPI", s.convert("PAYPALISHIRING", 4));
+    EXPECT_EQ("", s.convert("", 1));
+    EXPECT_EQ("", s.convert("", 3));
+    EXPECT_EQ("A", s.convert("A", 1));
+    EXPECT_EQ("AB", s.convert("AB", 1));
+    EXPECT_EQ("ABDC", s.convert("ABCD", 3));
 }
 
